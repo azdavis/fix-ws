7fee3b4821b0d9295cd13857e62592ed2418816b
Ariel Davis <ariel.z.davis@icloud.com>
2019-11-27 20:14:26 -0500

    Simplify file::{read, write}

--- src/file.rs
+++ src/file.rs
@@ -1,31 +1,11 @@
 //! Quick and dirty file operations.
 
 use crate::error::{Error, Result};
-use std::fs::File;
-use std::io::Read as _;
-use std::io::Write as _;
 
 pub fn read(fname: &str) -> Result<Vec<u8>> {
-	let mut f = match File::open(fname) {
-		Ok(f) => f,
-		Err(e) => return Err(Error::Io(fname.to_string(), e)),
-	};
-	let mut bs = Vec::new();
-	match f.read_to_end(&mut bs) {
-		Ok(f) => f,
-		Err(e) => return Err(Error::Io(fname.to_string(), e)),
-	};
-	Ok(bs)
+	std::fs::read(fname).map_err(|e| Error::Io(fname.to_string(), e))
 }
 
 pub fn write(fname: &str, bs: &[u8]) -> Result<()> {
-	let mut f = match File::create(fname) {
-		Ok(f) => f,
-		Err(e) => return Err(Error::Io(fname.to_string(), e)),
-	};
-	match f.write_all(bs) {
-		Ok(()) => (),
-		Err(e) => return Err(Error::Io(fname.to_string(), e)),
-	}
-	Ok(())
+	std::fs::write(fname, bs).map_err(|e| Error::Io(fname.to_string(), e))
 }
